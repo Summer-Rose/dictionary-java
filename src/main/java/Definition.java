c0624e74824333291154aa64738ce968507274af
Summer <student@epicodus.com>
2015-08-21 10:03:26 -0700

    Add getter methods and unit tests to Definition class

--- src/main/java/Definition.java
+++ src/main/java/Definition.java
@@ -9,4 +9,16 @@ public class Definition {
   public Definition(String definition) {
     mDefinition = definition;
   }
+
+  public String getDefinition() {
+    return mDefinition;
+  }
+
+  public int getId() {
+    return mId;
+  }
+  
+  public static ArrayList<Definition> all() {
+    return instances;
+  }
 }
--- src/test/java/DefinitionTest.java
+++ src/test/java/DefinitionTest.java
@@ -14,5 +14,15 @@ public class DefinitionTest {
 		assertEquals(true, testDefinition instanceof Definition);
 	}
 
+	@Test
+	public void Definition_getCorrectDefinition() {
+		Definition testDefinition = new Definition("The coolest person and hottest season");
+		assertEquals("The coolest person and hottest season", testDefinition.getDefinition());
+	}
 
+	@Test
+	public void Definition_returnsId_true() {
+		Definition testDefinition = new Definition("The coolest person and hottest season");
+		assertTrue(Definition.all().size() == testDefinition.getId());
+	}
 }
